ab48784741cfde15afaba5f4a530815a98f302f7
Seung Hyun Kim <seung.kim0708@gmail.com>
2024-08-31 18:48:37 -0700

    created a state for all the squares in Board component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
+import {useState} from 'react';
 import './App.css';
 import Square from './Square';
 //
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null))
   return (
     <main>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} />
+        <Square value={squares[1]}/>
+        <Square value={squares[2]}/>
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]}/>
+        <Square value={squares[4]}/>
+        <Square value={squares[5]}/>
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]}/>
+        <Square value={squares[7]}/>
+        <Square value={squares[8]}/>
       </div>
     </main>
   )
--- src/Square.js
+++ src/Square.js
@@ -1,13 +1,7 @@
-import {useState} from 'react';
 
-export default function Square() {
-    const [value, setValue] = useState(null)
-    //fill the Square component with an X when you click it
-    function handleClick() {
-        setValue('X')
-    }
-  
+export default function Square({value}) {
+
     return (
-       <button className="square" onClick={handleClick}>{value}</button>
+       <button className="square">{value}</button>
     )
   }
