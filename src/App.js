3cf0f5df2959729014b13ca466ebc40df5156f72
Seung Hyun Kim <seung.kim0708@gmail.com>
2024-09-02 12:25:54 -0700

    completed history feature

--- src/App.js
+++ src/App.js
@@ -3,25 +3,43 @@ import './App.css';
 import Board from './components/Board'
 
 export default function Game() {
-  const [xIsNext, setIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1]
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
 
-  //creating a hnandlePlay function that will be called by the Board component to update the game
   function handlePlay(nextSquares) {
-    setHistory([...history, nextSquares]); //creates a new array that contains all itesm in history, followed by nextSquares
-    //also want to toggle xIsNext, just as Board used to do:
-    setIsNext(!xIsNext);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
   }
 
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
   return (
-    <div className='game'>
-      <div className='game-board'>
+    <div className="game">
+      <div className="game-board">
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
-      <div className='game-info'>
-        <ol>{/*TODO*/}</ol>
+      <div className="game-info">
+        <ol>{moves}</ol>
       </div>
     </div>
-  )
+  );
 }
